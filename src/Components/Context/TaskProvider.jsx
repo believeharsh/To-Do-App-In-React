753959edbac5b903476ba3489cfc319d58a868d6
Believeharsh <bontinue@gmail.com>
2024-04-09 13:58:49 +0530

    completed

--- src/Components/Context/TaskProvider.jsx
+++ src/Components/Context/TaskProvider.jsx
@@ -1,41 +1,49 @@
-import React from 'react'
-import { TaskContext } from './TaskContext'
-import {v4 as uuidv4} from 'uuid'
-import {useState} from 'react'
+import React from "react";
+import { TaskContext } from "./TaskContext";
+import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
 
+const TaskProvider = ({ children }) => {
+  const InitialTasks = [
+    { id: uuidv4(), text: "Workout", completed: false },
+    { id: uuidv4(), text: "Book Reading", completed: false },
+    { id: uuidv4(), text: "Coding", completed: false },
+    { id: uuidv4(), text: "React Router Dom ", completed: false },
+  ];
+  const [Tasks, setTasks] = useState(InitialTasks);
 
-const TaskProvider = ({children}) => {
-    const InitialTasks = [
-        {id: uuidv4(), text:"Workout" , completed:false},
-        {id: uuidv4(), text:"Book Reading" , completed:false},
-        {id: uuidv4(), text:"Coding" , completed:false},
-        {id: uuidv4(), text:"React Router Dom " , completed:false}
-    ]
-   const [Tasks, setTasks] = useState(InitialTasks);
-
-   const addTask = (task) => {
+  const addTask = (task) => {
     let newTask = {
-        id: uuidv4(),
-        text: task,
-        completed: false,
-      };
-     setTasks([...Tasks, newTask])
-   }
-   const deleteTask = (taskid) => {
-     const filteredTasks = Tasks.filter((task) => task.id !== taskid);
-     setTasks(filteredTasks)
-   }
+      id: uuidv4(),
+      text: task,
+      completed: false,
+    };
+    if (task !== "") {
+      setTasks([...Tasks, newTask]);
+    } else {
+      alert("Input filed can't be empty");
+    }
+  };
+  const deleteTask = (taskid) => {
+    const filteredTasks = Tasks.filter((task) => task.id !== taskid);
+    setTasks(filteredTasks);
+  };
 
-   const EditTask = (taskId , newTask) => {
-    setTasks(Tasks.map((task) => task.id === taskId ? {...task , text: newTask} : task ))
-   }
-
+  const EditTask = (taskId, newTask) => {
+    setTasks(
+      Tasks.map((task) =>
+        task.id === taskId ? { ...task, text: newTask } : task
+      )
+    );
+  };
 
   return (
-   <TaskContext.Provider value={{Tasks, setTasks , addTask , EditTask , deleteTask}}>
+    <TaskContext.Provider
+      value={{ Tasks, setTasks, addTask, EditTask, deleteTask }}
+    >
       {children}
-   </TaskContext.Provider>
-  )
-}
+    </TaskContext.Provider>
+  );
+};
 
-export default TaskProvider
+export default TaskProvider;
--- src/Components/Tasks/AddTask.jsx
+++ src/Components/Tasks/AddTask.jsx
@@ -1,41 +1,41 @@
-import React from 'react'
-import { useState} from 'react'
-import UseTask from '../Context/UseTask'
-
+import React from "react";
+import { useState } from "react";
+import UseTask from "../Context/UseTask";
+import { FaPlus } from "react-icons/fa";
 const AddNewTask = () => {
-    const { addTask } = UseTask()
+  const { addTask } = UseTask();
 
-    const [inputValue, setinputValue] = useState('');
+  const [inputValue, setinputValue] = useState("");
 
-    const handleChange = (e) => {
-        setinputValue(e.target.value)
-    }
-    const handleSubmit = (e) => {
-      e.preventDefault();
-        addTask(inputValue)
-        setinputValue('')
-    }
+  const handleChange = (e) => {
+    setinputValue(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTask(inputValue);
+    setinputValue("");
+  };
 
   return (
-  <>
-    <form onSubmit={handleSubmit}>
-     <div className="flex mb-4" >
-     <input
-        type="text"
-        value={inputValue}
-        onChange={handleChange}
-        placeholder="Enter task"
-        className="w-full rounded border-gray-300 p-2 mr-2"
-      />
-      <button type="submit" 
-          className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
-      >Add Task
-      </button>
-     </div>
-      
-    </form>
-  </>
-  )
-}
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="flex mb-4">
+        
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleChange}
+            placeholder="Enter task"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none"
+          />
+          <button type="submit" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2 py-1 mx-[1.5px]  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          <FaPlus/>
+          </button>
+         
+        </div>
+      </form>
+    </>
+  );
+};
 
-export default AddNewTask
+export default AddNewTask;
